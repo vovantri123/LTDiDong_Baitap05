a389911a5af1544bf19ff9bf40429b22ad792a32
vovantri123 <vovantri204@gmail.com>
2025-03-08 09:22:13 +0700

    Tới bước 7

--- app/src/main/java/vn/iostar/baitap05/MainActivity.java
+++ app/src/main/java/vn/iostar/baitap05/MainActivity.java
@@ -1,24 +1,65 @@
 package vn.iostar.baitap05;
 
+import android.database.Cursor;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // Khai báo biến toàn cục
+    DatabaseHandler databaseHandler;
+    ListView listView;
+    ArrayList<NotesModel> arrayList;
+    NotesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Ánh xạ ListView và gọi adapter
+        listView = (ListView) findViewById(R.id.listView1);
+        arrayList = new ArrayList<>();
+        adapter = new NotesAdapter(this, R.layout.row_note,arrayList);
+        listView.setAdapter(adapter);
+
+        // Gọi hàm databaseSQLite
+        InitDatabaseSQLite();
+        createDatabaseSQLite();
+        databaseSQLite();
+
+
+
+
+    }
+
+    private void createDatabaseSQLite() {
+        // Thêm dữ liệu vào database
+        databaseHandler.QueryData("INSERT INTO Notes VALUES(null, 'Vi du SQLite 1')");
+        databaseHandler.QueryData("INSERT INTO Notes VALUES(null, 'Vi du SQLite 2')");
+    }
+
+    private void InitDatabaseSQLite() {
+        // Khởi tạo database
+        databaseHandler = new DatabaseHandler(this, "notes.sqlite", null, 1);
+
+        // Tạo bảng Notes
+        databaseHandler.QueryData("CREATE TABLE IF NOT EXISTS Notes(Id INTEGER PRIMARY KEY AUTOINCREMENT, NameNotes VARCHAR(200))");
+    }
+
+    private void databaseSQLite() {
+        // Lấy dữ liệu
+        Cursor cursor = databaseHandler.GetData("SELECT * FROM Notes");
+        while (cursor.moveToNext()) {
+            // Thêm dữ liệu vào arrayList
+            String name = cursor.getString(1);
+            int id = cursor.getInt(0);
+            arrayList.add(new NotesModel(id, name));
+            //Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
+        adapter.notifyDataSetChanged();
     }
 }
